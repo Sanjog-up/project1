90069117557bd3fb182ce8feb88b7655a853f834
Sanjog Aale <sanjogm940@gmail.com>
2026-09-24 23:18:33 +0545

    error path middleware

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 
 import userRoutes from "./routes/user.routes";
 import authRoutes from "./routes/auth.routes";
+import AppError from "./utils/appError.utils";
 
 const app = express();
 
@@ -16,9 +17,10 @@ app.use(cors());
 
 // Health check
 app.get("/", (req: Request, res: Response) => {
-  res.json({
+  res.status(200).json({
     message: "server is up and running",
     status: "success",
+    success: true,
   });
 });
 
@@ -26,5 +28,12 @@ app.get("/", (req: Request, res: Response) => {
 // Highlight: these mounts were missing before
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
+
+//! error path middleware
+app.use((req: Request, res: Response) => {
+  const message = `Can't find ${req.originalUrl} on this server!`;
+  throw new AppError(message, 404);
+});
+
 
 export default app;
